dbbe1ba7209c70aa1969a15e3865bd27854f011f
clin19emich <linchristina.03@gmail.com>
2024-10-26 01:45:03 -0400

    Changes made to UI colors, code layout, and added remaining profile fields, and misc.
    Added the remaining profile fields listed in the tasks.
    Added a back button on sign up page and create profile page(Back to login)
    Changed the code layout to be more readable and change UI colors to match other UI screens, will be changed again to match home landing page.

--- src/pages/CreateProfilePage.tsx
+++ src/pages/CreateProfilePage.tsx
@@ -1,43 +1,163 @@
 import React, {useState} from "react";
 import LandingPage from "./LandingPage";
+import Start from "../start";
+
 
 const CreateProfilePage: React.FC = () => {
-    const [username, setfirstName] = useState(''); 
-    const [password, setlastName] = useState('');
-    const [confirmpassword, setAtt1] = useState('');
+    const [firstname, setfirstName] = useState(''); 
+    const [lastname, setlastName] = useState('');
+
+    // Set Image
+    const [images, setImages] = useState ('');
+    // Set User Age
+    const [userage, setuserAge] = useState ('');
+    // Set Bio
+    const [bio,setBio] = useState('')
+    // Set Gender
+    const [gender,setGender] = useState ('');
+    // Set Preferred Gender
+    const [genderPreferrence,setgenderPreferrence] = useState('');
+    // Set preferred Age
+    const [agePreferrence,setagePreferrence] = useState('');
+    // Set Major if app
+    const [major, setMajor] = useState ('');
 
     return (
       <div>
         <h1>Create Profile Page</h1>
-        <p>This is the Create Profile page.</p>
+        <p></p>
         <form>
-            <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
+            <label 
+                htmlFor="firstName" 
+                className="block text-sm font-medium leading-6 text-gray-900">
                 Enter your First name
             </label>
-            <input type="text" id="firstName" name="firstName" value={username} onChange={(e) => setfirstName(e.target.value)}                  
-                   className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            <input 
+                type="text" 
+                id="firstName" 
+                name="firstName"
+                value={firstname} 
+                onChange={(e) => setfirstName(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
             />
     
-            <label htmlFor="elastNam" className="block text-sm font-medium leading-6 text-gray-900">
+            <label 
+                htmlFor="lastName" 
+                className="block text-sm font-medium leading-6 text-gray-900">
                 Enter your Last Name
             </label>
-            <input type="text" id="lastName" name="lastName" value={password} onChange={(e) => setlastName(e.target.value)}                  
-                   className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            <input 
+                type="text" 
+                id="lastName" 
+                name="lastName"
+                value={lastname} 
+                onChange={(e) => setlastName(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
             />
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                Profile Attribute
+            {/* EDIT Images URGENT */}
+            <label 
+                htmlFor="text" 
+                className="block text-sm font-medium leading-6 text-gray-900">
+                Images
             </label>
-            <input type="text" id="att1" name="att1" value={confirmpassword} onChange={(e) => setAtt1(e.target.value)}                  
-                   className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            <input 
+                type="text" 
+                id="att1" 
+                name="att1" 
+                value={images} 
+                onChange={(e) => setImages(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
             />
-            
+            <label 
+                htmlFor="userage" 
+                className="block text-sm font-medium leading-6 text-gray-900">
+                Age
+            </label>
+            <input
+                type="text" 
+                id="userage" 
+                name="userage" 
+                value={userage} 
+                onChange={(e) => setuserAge(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+            />
+            <label 
+                htmlFor="text" 
+                className="block text-sm font-medium leading-6 text-gray-900">
+                Bio
+            </label>
+            <input 
+                type="text" 
+                id="att1" 
+                name="att1" 
+                value={bio} 
+                onChange={(e) => setBio(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+            />
+            <label 
+                htmlFor="text" 
+                className="block text-sm font-medium leading-6 text-gray-900">
+                Gender
+            </label>
+            <input 
+                type="text" 
+                id="att1" 
+                name="att1" 
+                value={gender} 
+                onChange={(e) => setGender(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+            />
+            <label htmlFor="text" className="block text-sm font-medium leading-6 text-gray-900">
+                Preferred Gender
+            </label>
+            <input 
+                type="text" 
+                id="att1" 
+                name="att1" 
+                value={genderPreferrence} 
+                onChange={(e) => setgenderPreferrence(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+            />
+            <div className="flex items-center space-x-4">
+                <label htmlFor="text" className="block text-sm font-medium leading-6 text-gray-900">
+                    Age Preferrence:
+                </label>
+                <select 
+                    id="age" 
+                    name="age" 
+                    onChange={(e) => setagePreferrence(e.target.value)}
+                    className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6">
+                        <option value="">--Select Age--</option>
+                        <option value="18-24">18-24</option>
+                        <option value="25-34">25-34</option>
+                        <option value="35-44">35-44</option>
+                        <option value="45-54">45-54</option>
+                        <option value="55-64">55-64</option>
+                        <option value="65+">65+</option>
+                </select>
+            </div>
+            <label 
+                htmlFor="text" 
+                className="block text-sm font-medium leading-6 text-gray-900">
+                Major (if applicable)
+            </label>
+            <input 
+                type="text" 
+                id="att1" 
+                name="att1" 
+                value={major} 
+                onChange={(e) => setMajor(e.target.value)}                  
+                className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+            />
         </form>
         <a href="/LandingPage">
-                <button type="submit" className="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6
-                        text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600" >
-                    Start Connecting!
-                </button>
-            </a>
+            <button 
+            type="submit" 
+            className="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600" >
+            Start Connecting!
+            </button>
+        </a>
+        <a href="/">Back to Login</a>
         
       </div>
     );
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -3,45 +3,94 @@ import React ,{ useState }from "react";
 import CreateProfilePage from "./CreateProfilePage";
 
 const SignUpPage: React.FC = () => {
+    const [email, setEmail] = useState(''); 
     const [username, setUsername] = useState(''); 
     const [password, setPassword] = useState('');
     const [confirmpassword, confirmPassword] = useState('');
 
+
     return (
       <div title="website-layout" className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h1>Sign Up Page</h1>
-       
-        <form>
-            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                Enter your new username
-            </label>
-            <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}                  
-                   className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-    
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                Enter your new password
-            </label>
-            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}                  
-                   className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                Re-enter your new password
-            </label>
-            <input type="password" id="password" name="password" value={confirmpassword} onChange={(e) => confirmPassword(e.target.value)}                  
-                   className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-        </form>
-        
-                  
-              
-        <a href="/CreateProfilePage">
-            <button type="submit" className="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6
-                    text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600" >
-                Proceed to Creating your Profile
-            </button>
-        </a>
+            <h1>Sign Up Page</h1>
+            <div>
+                <form>
+                    {/* Email Field */}
+                    <label 
+                        htmlFor="email" 
+                        className="block text-sm font-medium leading-6 text-gray-900">
+                        Enter your email
+                    </label>
+                        <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        required 
+                        pattern=".+@emich\.edu$" 
+                        title="Invalid email address, must be EMU" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}                  
+                        className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+
+                    {/* Username Field */}
+                    <label 
+                        htmlFor="username" 
+                        className="block text-sm font-medium leading-6 text-gray-900">
+                        Enter a username
+                    </label>
+                        <input 
+                        type="username" 
+                        id="username" 
+                        name="username" 
+                        required 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)}                  
+                        className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+
+                    {/* Password Field */}
+                    <label 
+                        htmlFor="password" 
+                        className="block text-sm font-medium leading-6 text-gray-900">
+                        Enter your new password
+                    </label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password" 
+                            required 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)}                  
+                            className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    {/* Re-confirm Password Field */}
+                    <label 
+                        htmlFor="password" 
+                        className="block text-sm font-medium leading-6 text-gray-900">
+                        Re-enter your new password
+                    </label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password" 
+                            required 
+                            value={confirmpassword} 
+                            onChange={(e) => confirmPassword(e.target.value)}                  
+                            className="block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                </form>
+            </div>
+                <a href="/CreateProfilePage">
+                    <button 
+                        type="submit" 
+                        className="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600" >
+                        Proceed to Creating your Profile
+                    </button>
+                </a>
+                <a href="/">
+                    Back to Login
+                </a>
         </div>
       </div>
       
